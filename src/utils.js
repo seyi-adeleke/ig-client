88b3f7316cd7f74232ffba6b3bc526b05aac36eb
seyi-adeleke <seyiadeleke42@yahoo.com>
2019-07-01 16:47:13 +0100

    add axios call, api state handling abbl.

--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,7 @@ export const fetchRandomEmoji = () => {
 
     return emojis[Math.floor(Math.random() * emojis.length)];
 }
+
+export const validUrl = (url) => {
+    return !url.startsWith('https://www.instagram.com/');
+}
